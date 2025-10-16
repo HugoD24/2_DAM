b9c6d23c5ab2f4bdc679518df11d52cdffda7118
HugoD24 <4048314@alu.murciaeduca.es>
2025-10-16 11:48:43 +0200

    Estructura actividades psp

---
+++ PSP/HugoDesiderio_Act_01Multiproceso/HugoDesiderioGarcia_Act_01Multiproceso/src/main/java/ejercicio5/Aplicación1.java
@@ -0,0 +1,21 @@
+package ejercicio5;
+
+/**
+ *      Crea dos aplicaciones
+ *          Aplicación 1
+ *              i. Le va pidiendo números enteros al usuario hasta que este le introduzca un 0 o un número negativo.
+ *              ii. Todos estos números se los enviará a una Aplicación 2 que le deberá una serie de
+ *                  información que deberá mostrar por terminal. Estos datos se enviarán para que
+ *                  Aplicación 2 los reciba por su entrada estándar.
+ *          Aplicación 2
+ *              i. Recibirá un número indeterminado de números mayores a 0 y deberá imprimir por
+ *                 consola los siguientes resultados
+ *                      1. Dos números mayores
+ *                      2. Menor número
+ *                      3. Media
+ *                      4. Número más repetido.
+ *
+ */
+
+public class Aplicación1 {
+}
